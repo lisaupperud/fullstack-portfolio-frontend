f74a7301bd60e06a2ac38aafd2e26ee8d4be9553
liluppis <lisa.upp@hotmail.com>
2025-10-27 15:22:26 +0100

    add: navbar component with basic styling and structure

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="links">
+        <a href="">Home</a>
+        <a href="">Projects</a>
+        <a href="">CV</a>
+        <a href="">Contact</a>
+      </div>
+    </div>
+  )
+}
