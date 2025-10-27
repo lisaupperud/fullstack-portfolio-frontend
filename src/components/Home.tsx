5cf43f54a3d63cb544a972cc5f905f176dc2db7e
liluppis <lisa.upp@hotmail.com>
2025-10-27 19:31:16 +0100

    add: bg picture for home component & fonts for entire app

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,21 @@
-//import lisaupperud from "../assets/lisaupperud.jpeg"
+import lisaupperud from "../assets/lisaupperud.jpeg"
 import "./Home.css"
 
 export default function Home() {
-  return <div></div>
+  return (
+    <div className="home-container">
+      <div className="introduction">
+        <img src={lisaupperud} alt="Lisa Upperud smiling to the camera" />
+        <div className="introduction-text">
+          <h1>WELCOME</h1>
+          <p>
+            bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+            bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+            bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+            bla bla bla bla bla bla bla bla bla bla
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
