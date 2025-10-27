3ec9776d812853a015e11e235a46e1b313cb71f2
liluppis <lisa.upp@hotmail.com>
2025-10-27 21:09:08 +0100

    add: basic styling and structure to about - needs work

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,8 +2,11 @@ import "./About.css"
 
 export default function About() {
   return (
-    <div>
+    <div className="about-container">
       <h1>About Me</h1>
+      <div className="about-content">
+        <p>Content</p>
+      </div>
     </div>
   )
 }
