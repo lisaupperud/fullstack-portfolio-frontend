d6a17e8557255521a226ec50e1c4aae48daf6f42
liluppis <lisa.upp@hotmail.com>
2025-10-27 14:40:11 +0100

    add: base for home component for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css"
+import Home from "./components/Home"
 import ProjectList from "./components/ProjectList"
 
 function App() {
   return (
     <div>
+      <Home />
       <h1>My Projects</h1>
       <ProjectList />
     </div>
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import lisaupperud from "../assets/lisaupperud.jpeg"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <img src={lisaupperud} alt="Picture of Lisa smiling" />
+      <p>This is the home page of my portfolio website.</p>
+    </div>
+  )
+}
