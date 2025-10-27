1098ba6a7421f6c5bd383ce4a57c257369ef8f08
liluppis <lisa.upp@hotmail.com>
2025-10-27 19:09:08 +0100

    remove: css for components, refactor: root css to get a clean slate

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,29 +1,6 @@
-import lisaupperud from "../assets/lisaupperud.jpeg"
+//import lisaupperud from "../assets/lisaupperud.jpeg"
 import "./Home.css"
 
 export default function Home() {
-  return (
-    <div className="home-page">
-      <h1>Welcome to My Portfolio</h1>
-
-      <div className="introduction">
-        <img
-          src={lisaupperud}
-          alt="Picture of Lisa smiling"
-          className="profile-pic"
-        />
-        <div className="intro-text">
-          <p>Introduction</p>
-          <p>
-            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
-            ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
-          </p>
-        </div>
-      </div>
-
-      <div className="next-page">
-        <p>Check out my projects!</p>
-      </div>
-    </div>
-  )
+  return <div></div>
 }
