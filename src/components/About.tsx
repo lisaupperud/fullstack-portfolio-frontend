91bf96ac6a3cdbb1a777fa3eb8a73905ca55111f
liluppis <lisa.upp@hotmail.com>
2025-11-20 15:27:55 +0100

    refactor: about component and styling to match other components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,9 +3,14 @@ import "./About.css"
 export default function About() {
   return (
     <div className="about-container">
-      <h1>About Me</h1>
+      <div>
+        <h1 id="title">ABOUT ME</h1>
+        <p id="copy">SHORT, BOLD COPY ABOUT SECTION</p>
+        <p id="short-copy">Some explanation...</p>
+      </div>
+
       <div className="about-content">
-        <p></p>
+        <p>CONTENT EXAMPLE</p>
       </div>
     </div>
   )
