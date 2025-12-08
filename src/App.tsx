0e2851e5a53d89290c705fb8e554d06fb78fd502
liluppis <lisa.upp@hotmail.com>
2025-12-08 11:39:21 +0100

    remove: NavigationLine in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,11 @@ import Navbar from "./components/Navbar"
 import ProjectList from "./components/Project"
 import Work from "./components/Work"
 import Contact from "./components/Contact"
-import NavigationLine from "./components/navigation-line/navigationLine"
 
 function App() {
   return (
     <div className="app">
       <Navbar />
-      <NavigationLine
-        sections={[
-          "section-projects",
-          "section-work",
-          "section-about",
-          "section-contact",
-        ]}
-      />
       <Home />
       <ProjectList />
       <Work />
