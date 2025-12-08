41ca7302c06ecd678f140266650897f1e145fea9
liluppis <lisa.upp@hotmail.com>
2025-12-08 13:43:25 +0100

    add: styling + copy in contact comp

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,5 +1,8 @@
 import "./Contact.css"
 import { useToggle } from "../../hooks/useToggle"
+import arrow from "../../assets/arrow-right.png"
+import linkedin from "../../assets/linkedin.png"
+import mail from "../../assets/mail.png"
 
 export default function Contact() {
   const { value: showContact, toggle } = useToggle()
@@ -10,15 +13,35 @@ export default function Contact() {
         <h1 id="title" onClick={toggle}>
           CONTACT
         </h1>
-        <p id="copy">SHORT, BOLD COPY ABOUT SECTION</p>
-        <p id="short-copy">Some explanation...</p>
-      </div>
+        <p id="copy">INTRIGUED? WANT TO KNOW MORE?</p>
+        <p id="short-copy">Let's get in contact!</p>
 
-      {showContact && (
-        <div className="contact-content">
-          <p>CONTENT EXAMPLE</p>
-        </div>
-      )}
+        {showContact && (
+          <div className="contact-content">
+            <div className="contact-links">
+              <img
+                src={arrow}
+                alt="Purple arrow pointing to the right"
+                id="arrows"
+              />
+              <a href="">
+                <img src={linkedin} alt="LinkedIn Logo" id="icons" />
+              </a>
+            </div>
+            <div className="contact-links">
+              <img
+                src={arrow}
+                alt="Purple arrow pointing to the right"
+                id="arrows"
+              />
+              <a href="mailto:lisa.upp@hotmail.com">
+                <img src={mail} alt="E-mail Icon" id="icons" />
+              </a>
+            </div>
+            <h1 id="thanks">THANKS FOR CHECKING OUT MY PORTFOLIO!</h1>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
