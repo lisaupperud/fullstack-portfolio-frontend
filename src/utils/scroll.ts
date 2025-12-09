bc11049dca19880667ed398ad32cce72f639d247
liluppis <lisa.upp@hotmail.com>
2025-12-09 11:22:45 +0100

    small fix: scrollToId() to scroll to center instead of start

--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -2,7 +2,7 @@ export function scrollToId(id: string) {
   setTimeout(() => {
     const scrollElement = document.getElementById(id)
     if (scrollElement) {
-      scrollElement.scrollIntoView({ behavior: "smooth", block: "start" })
+      scrollElement.scrollIntoView({ behavior: "smooth", block: "center" })
     }
   }, 200)
 }
