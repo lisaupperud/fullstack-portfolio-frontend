41e4471f54e5d42f9f98b1f18c3155169c1bf2e5
liluppis <lisa.upp@hotmail.com>
2026-01-07 13:41:45 +0100

    refactor: make loading function prettier

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -14,44 +14,58 @@ import { useToggle } from "../../hooks/useToggle"
 export default function ProjectList() {
   const [projects, setProjects] = useState<Project[]>([])
   const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<boolean>(false)
   const { value: showProjects, toggle } = useToggle()
 
   useEffect(() => {
     fetchProjects()
-      .then((data) => setProjects(data))
-      .catch((error) => console.error("Error fetching projects:", error))
+      .then((data) => {
+        setProjects(data)
+        setError(false)
+      })
+      .catch((error) => {
+        console.error("Error fetching projects:", error)
+        setError(true)
+      })
       .finally(() => setLoading(false))
   }, [])
 
-  if (loading) {
-    return <div>Loading projects...</div>
-  }
-
   return (
     <div className="project-container" id="projects">
-      <div>
-        <h1 id="title" onClick={toggle}>
-          PROJECTS
-        </h1>
+      <div className="project-header" onClick={toggle}>
+        <div className="title-with-arrow">
+          <h1 id="title">PROJECTS</h1>
+        </div>
         <div className="underline-wrapper">
           <div className="underline" />
         </div>
-
         <p id="copy" className="desktop-only">
           A SELECTION OF THE PROJECTS THAT I AM MOST PROUD OF.
         </p>
-        {/*<p id="short-copy" className="desktop-only">
-          A showcase of the projects that show a wide range of competence and
-          knowledge. Scroll thorugh, read and visit the Repositories to get a
-          deeper look into the codebase. Click the tags to sort projects based
-          on technology used.
-        </p>*/}
         <div className="mobile-only-wrapper">
-          <p className="mobile-only">Swipe to see the projects</p>
+          <p className="mobile-only">
+            Click the title & swipe to see the projects
+          </p>
         </div>
       </div>
 
-      {showProjects && (
+      {loading && !error && <div>...</div>}
+
+      {error && (
+        <div className="loading-placeholder">
+          <p
+            style={{
+              fontSize: "1.5rem",
+              color: "black",
+              textAlign: "center",
+            }}
+          >
+            PROJECTS LOADING...
+          </p>
+        </div>
+      )}
+
+      {!loading && !error && showProjects && (
         <div className="project-content">
           {projects.map((project) => (
             <div key={project.id} className="project-list">
